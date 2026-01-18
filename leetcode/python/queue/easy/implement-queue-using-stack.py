1c2821d587a6dd4dfbbd0569a00db021a7f8b171
Ukashatu40 <ukashatuabdullahi40@gmail.com>
2026-01-18 20:16:49 +0100

    solved: implement queue using stack problem in python

---
+++ leetcode/python/queue/easy/implement-queue-using-stack.py
@@ -0,0 +1,36 @@
+class MyQueue:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.stack_in = []
+        self.stack_out = []
+
+    def push(self, x: int) -> None:
+        """
+        Push element x to the back of queue.
+        """
+        self.stack_in.append(x)
+
+    def pop(self) -> int:
+        """
+        Removes the element from in front of queue and returns that element.
+        """
+        self.peek()  # Ensure stack_out has the current front element
+        return self.stack_out.pop()
+
+    def peek(self) -> int:
+        """
+        Get the front element.
+        """
+        if not self.stack_out:
+            while self.stack_in:
+                self.stack_out.append(self.stack_in.pop())
+        return self.stack_out[-1]
+
+    def empty(self) -> bool:
+        """
+        Returns whether the queue is empty.
+        """
+        return not self.stack_in and not self.stack_out
