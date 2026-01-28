25aa9323178a2bfc3d822a59230262cc3ac137f9
Ukashatu40 <ukashatuabdullahi40@gmail.com>
2026-01-28 19:16:22 +0100

    solved: kth largest element in a  stream problem in python

---
+++ leetcode/python/priority-queue/easy/kth-largest-element-in-a-stream.py
@@ -0,0 +1,16 @@
+class Solution:
+
+    def __init__(self, k: int, nums: list[int]):
+        import heapq
+        self.k = k
+        self.min_heap = nums
+        heapq.heapify(self.min_heap)
+        while len(self.min_heap) > k:
+            heapq.heappop(self.min_heap)
+
+    def add(self, val: int) -> int:
+        import heapq
+        heapq.heappush(self.min_heap, val)
+        if len(self.min_heap) > self.k:
+            heapq.heappop(self.min_heap)
+        return self.min_heap[0]
