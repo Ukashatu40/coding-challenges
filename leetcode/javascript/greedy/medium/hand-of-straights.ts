69f464a068ba893f1e3be79b5604cfc6cbb30edc
Ukashatu40 <ukashatuabdullahi40@gmail.com>
2026-03-31 18:10:25 +0100

    solved: hand of straights problem in TypeScript

---
+++ leetcode/javascript/greedy/medium/hand-of-straights.ts
@@ -0,0 +1,28 @@
+function isHandOfStraights(hand: number[], groupSize: number): boolean {
+  const n = hand.length;
+  if (n % groupSize !== 0) {
+    return false;
+  }
+
+  const countMap = new Map<number, number>();
+  for (const card of hand) {
+    countMap.set(card, (countMap.get(card) || 0) + 1);
+  }
+
+  const uniqueCards = Array.from(countMap.keys()).sort((a, b) => a - b);
+
+  for (const card of uniqueCards) {
+    const count = countMap.get(card)!;
+    if (count > 0) {
+      for (let i = 0; i < groupSize; i++) {
+        const nextCard = card + i;
+        if (!countMap.has(nextCard) || countMap.get(nextCard)! < count) {
+          return false;
+        }
+        countMap.set(nextCard, countMap.get(nextCard)! - count);
+      }
+    }
+  }
+
+  return true;
+}
